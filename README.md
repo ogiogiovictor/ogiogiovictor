13ef87f2f850e422fe84a4db3f2d7668a4739966
OgiogioVictor <ogiogiovictor@gmail.com>
2022-10-17 14:30:30 -0600

    fixed readme issues

--- README.md
+++ README.md
@@ -14,17 +14,32 @@
 <h3>🛠 Tech Stack</h3>
 
 - 🌐 &nbsp; <i> Javascript | PHP | React | Vue | Bootstrap | HTML | CSS | Dart | Ruby | </i>
-- 🛢 &nbsp; <i>MySQL | Firebase | Postgre </i>
+- 🛢 &nbsp; <i>MySQL | Firebase | Postgre | Mssql </i>
 - 🔧 &nbsp; <i>Visual Studio code | Git | </i>
 - 🖥 &nbsp; <i>Eslint | Stylint | Webhint</i>
+
+<h3>🛠🖥  Frameworks and Libraries</h3>
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+![Webpack](https://img.shields.io/badge/webpack-%238DD6F9.svg?style=for-the-badge&logo=webpack&logoColor=black)
+![Laravel PHP](https://img.shields.io/badge/laravel.php-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)
+![NPM](https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white)
+
+<h3>🛢🛠🖥 Education</h3>
+![FreeCodeCamp](https://img.shields.io/badge/Freecodecamp-%23123.svg?&style=for-the-badge&logo=freecodecamp&logoColor=green)
+![MDN Web Docs](https://img.shields.io/badge/MDN_Web_Docs-black?style=for-the-badge&logo=mdnwebdocs&logoColor=white)
+![Udemy](https://img.shields.io/badge/Udemy-A435F0?style=for-the-badge&logo=Udemy&logoColor=white)
 
 <br>
 
 <img align="center" src="https://github-readme-stats.vercel.app/api?username=ogiogiovictor&include_all_commits=true&count_private=true&show_icons=true&line_height=20&title_color=7A7ADB&icon_color=2234AE&text_color=D3D3D3&bg_color=0,000000,130F40" alt="ogiogiovictor Github Stats">
 
 </br>
-
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ogiogiovictor&layout=compact&text_color=daf7dc&bg_color=151515)](https://github.com/ogiogiovictor/)
+
+</br>
+[![GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=ogiogiovictor&theme=dark)](https://git.io/streak-stats)
+
 
 <h3 align="center"> 🤝🏻 Connect with Me </h3>
 
---
+++ m.js
@@ -0,0 +1,77 @@
+![Microverse](https://img.shields.io/badge/Microverse-blueviolet)
+![Header](./github-header-image.png)
+
+![Quote](https://github-readme-quotes.herokuapp.com/quote?theme=dark)
+
+<details>
+ <summary>Learn more about me 😄!</summary>
+ <ul>
+  <li>I'm currently learning how to be a professional software developer with Microverse!</li>
+  <li>I’m available for new opportunities and projects you'd need any help with!</li>
+  <li>My projects are available at <a href="https://ulisesedu7.github.io/Portafolio/" target=”_blank”>@ulisesedu7</a></li>
+  <li>⚡I like a lot: 🎹Music | 🎮Gaming | 🥊Boxing | ⚽Soccer | 🎥Movies & Series | 🍔Food</li>
+ </ul>
+</details>
+
+## Technologies 💻
+
+### Front-End 🖥️
+ ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+ ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+ ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+### Back-End 💾
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+  
+### Frameworks and Libraries
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+![Webpack](https://img.shields.io/badge/webpack-%238DD6F9.svg?style=for-the-badge&logo=webpack&logoColor=black)
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)
+![NPM](https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white)
+
+
+### Databases
+![Postgres](https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white)
+![MySQL](https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white)
+![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)
+
+### Tools
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+![ESLint](https://img.shields.io/badge/ESLint-4B3263?style=for-the-badge&logo=eslint&logoColor=white)
+![Microsoft](https://img.shields.io/badge/Microsoft-0078D4?style=for-the-badge&logo=microsoft&logoColor=white)
+
+### Education
+![edX](https://img.shields.io/badge/edX-%2302262B.svg?style=for-the-badge&logo=edX&logoColor=white)
+![FreeCodeCamp](https://img.shields.io/badge/Freecodecamp-%23123.svg?&style=for-the-badge&logo=freecodecamp&logoColor=green)
+![MDN Web Docs](https://img.shields.io/badge/MDN_Web_Docs-black?style=for-the-badge&logo=mdnwebdocs&logoColor=white)
+![Udemy](https://img.shields.io/badge/Udemy-A435F0?style=for-the-badge&logo=Udemy&logoColor=white)
+
+## Stats 📊
+[![Ulises's GitHub stats](https://github-readme-stats.vercel.app/api?username=ulisesedu7&count_private=true&show_icons=true&theme=radical)](https://github.com/ulisesedu7)
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ulisesedu7&langs_count=6&layout=compact)](https://github.com/ulisesedu7)
+
+[![GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=ulisesedu7&theme=dark)](https://git.io/streak-stats)
+
+## Contact 📫
+
+<div>
+  <div>
+    &ensp;&ensp;&ensp;&ensp;&ensp; <a  href="https://www.linkedin.com/in/ulises-largaespada-45570b1a4/" target="_blank">
+      <img src="https://img.shields.io/badge/Linked%20In-0A66C2.svg?style=for-the-badge&logo=linkedin&logoColor=white" alt="lindedIn"/>
+    </a>
+    &ensp;&ensp;&ensp;&ensp;&ensp; <a href="https://twitter.com/LongswordMusic" target="_blank">
+     <img src="https://img.shields.io/badge/Twitter-1DA1F2.svg?style=for-the-badge&logo=twitter&logoColor=white" alt="twitter"/>
+    </a>
+    &ensp;&ensp;&ensp;&ensp;&ensp; <a href="mailto:ulisesedu7@gmail.com?subject=Feedback%20From%20Github&body=Hello," target="_blank">
+    <img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt="email"/>
+  </a>
+  </div>
+</div>
+
+## 😂 Here is a random joke that'll make you laugh!
+![Jokes Card](https://readme-jokes.vercel.app/api)
